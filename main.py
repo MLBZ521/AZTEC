1a1284f522f6e31ad5e45c48afa2fd0629433469
Zack T <MLBZ521@gmail.com>
2022-01-07 14:42:39 -0700

    Added CLI switches for database management
    + Added command line switch to support specifying database file and whether or not to reset the devices table within the database
    + Listed Python path in shebang

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+#!/opt/ManagedFrameworks/Python.framework/Versions/Current/bin/python3
 
+import argparse
 import os
 import threading
+
+from distutils.util import strtobool
 
 from AZTEC import cfgutil, utilities
 from AZTEC.db_utils import init_db
@@ -8,6 +12,24 @@ from AZTEC.db_utils import init_db
 
 def main():
     """Starts the main AZTEC process"""
+
+    ##################################################
+    # Define Script Parameters
+
+    parser = argparse.ArgumentParser(
+        description="This script utilizes the Apple Configurator CLI Tools (`cfgutil`) to automate the provisioning process of Apple Devices.")
+    parser.add_argument(
+        "--database", "-d", 
+        default="devices.db", 
+        help="Specify a database file to create or use if it already exists.", 
+        required=False)
+    parser.add_argument("--reset", "-r", 
+        choices=("true", "false", "yes", "y", "no", "n"),
+        help="If the specified database already exists, optionally purge the devices table.", 
+        required=False,
+        type=str.lower)
+
+    args, unknown = parser.parse_known_args()
 
     print("\n__{b}A{e}utomated {b}Z{e}ero {b}T{e}ouch {b}E{e}nrollment {b}C{e}onfigurator__\n".format(
         b="\033[1m", e="\033[0m"))
@@ -18,21 +40,26 @@ def main():
     print("Available Disk Space:  {}\n".format(free))
 
     # Check if the database file currently exists
-    if os.path.isfile("devices.db"):
+    if os.path.isfile(args.database):
+        reset = True
 
-        results = utilities.query_user_yes_no(
+        if args.reset is None:
+            reset = utilities.query_user_yes_no(
             "The devices database already exists, would you like to purge the existing content?")
 
-        if results:
+        if not strtobool(args.reset.lower()):
+            reset = False
+
+        if strtobool(args.reset.lower()) and reset:
             print("Purging previous content...\n")
-            init_db("devices.db")
+            init_db(args.database)
 
             # Back up previous log files
             utilities.log_backup()
 
     else:
 
-        init_db("devices.db")
+        init_db(args.database)
 
     try:
 
@@ -49,7 +76,7 @@ def main():
         print("System ready to accept devices!\n")
 
         utilities.execute_process(
-            "cfgutil exec --on-attach {path}/sh-attach.sh --on-detach {path}/sh-detach.sh".format(
+            "cfgutil exec --on-attach '{path}/sh-attach.sh' --on-detach '{path}/sh-detach.sh'".format(
                 path = utilities.module_directory
             ) 
         )
